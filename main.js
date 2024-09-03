7acc8652b29ded8e4f22b5621ae1b44d8ed9f90c
goreckinha <130074812+goreckinha@users.noreply.github.com>
2024-09-03 11:07:09 -0300

    mmoikpokp

--- main.js
+++ main.js
@@ -7,8 +7,8 @@ const textoResultado = document.querySelector(".texto-resultado");
 
 const perguntas = [
     {
-        enunciado: "Pergunta 1",
-        alternativas: ["aternativa1","alternativa2","alternativa3"]
+        enunciado: "Qual a data da independência do Brasil",
+        alternativas: ["07 de Setembro","15 de Novembro","21 de Abril"]
     },
     {
         enunciado: "Pergunta 2",
@@ -22,4 +22,16 @@ const perguntas = [
             enunciado: "Pergunta 4",
             alternativas: ["aternativa1","alternativa2","alternativa3"]
     },
+
+
 ]
+
+
+let atual = 0;
+let perguntaAtual; 
+
+function mostraPergunta(){
+perguntaAtual = perguntas[atual];
+
+
+}
